799bc3377d837a8dd4cbf38b57704cee3df118ff
Nipat <nipatchapakdee@gmail.com>
2025-04-29 13:11:48 +0700

    add new logic logout button

--- frontend/src/components/LogoutButton.tsx
+++ frontend/src/components/LogoutButton.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../store/hook" 
+import { logout } from "@/store/authSlice";
 
 interface LogoutButtonProps {
   className?: string; 
@@ -6,8 +8,10 @@ interface LogoutButtonProps {
 
 export default function LogoutButton({ className }: LogoutButtonProps) {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch()
 
   const handleLogout = () => {
+    dispatch(logout());
     localStorage.removeItem("token");
     localStorage.removeItem("role");
     navigate("/");

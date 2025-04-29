5914ba0f2e98de7f61f0e3c4729d2775470417f4
Nipat <nipatchapakdee@gmail.com>
2025-04-29 10:51:36 +0700

    change baseURL to dev mode

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://eugene-hypothesis-doug-guide.trycloudflare.com/",
+  baseURL: "http://localhost:3001/",
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });

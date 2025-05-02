dcb42bf90606d5ac69d8892f3f53cfd60ac0fdbf
Nipat <nipatchapakdee@gmail.com>
2025-05-03 01:11:17 +0700

    ./././

--- frontend/src/schemas/userSchema.ts
+++ frontend/src/schemas/userSchema.ts
@@ -5,7 +5,7 @@ export const UserResponseSchema = z.object({
   username: z.string().min(1, { message: "Username is required" }),
   email: z.string().email({ message: "Invalid email address" }),
   role_id: z.number(),
-  role: z.enum(["SAAS_SUPER_ADMIN", "BRANCH_ADMIN", "STAFF", "USER"]),
+  role: z.string(),
   createdAt: z.string().datetime({ message: "Invalid createdAt format" }).optional(),
   updatedAt: z.string().datetime({ message: "Invalid updatedAt format" }).optional(),
   deletedAt: z.string().datetime({ message: "Invalid deletedAt format "}).nullable().optional(),
